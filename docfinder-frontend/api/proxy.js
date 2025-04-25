05ebfc4897776498507c6bfaac3851559dee3e07
CyberRift <107774155+Dreadwolf26@users.noreply.github.com>
2025-04-25 09:00:38 -0400

    Update proxy.js
    Removing allow credentials. Meant to remove when implementing allow origin.

--- docfinder-frontend/api/proxy.js
+++ docfinder-frontend/api/proxy.js
@@ -3,7 +3,6 @@
 
 export default async function handler(req, res) {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', 'https://doc-finder-ecru.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
